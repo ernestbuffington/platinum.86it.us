750ea6818486dae16d5974127a9d7c544b0dfb6c
Ernest Buffington <ernest.buffington@gmail.com>
2023-03-05 14:55:51 -0500

    Update rector.php

--- rector.php
+++ rector.php
@@ -8,16 +8,17 @@ use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
+		  __DIR__ . '/includes/db/mysqli.php',
 		  //__DIR__ . '/includes/nukesentinel.php',
-		  //__DIR__ . '/mainfile.php',
+		  //__DIR__ . '/mainfile.php', PHP 8.2.3
 		  //__DIR__ . '/modules/Forums/common.php',
-		  __DIR__ . '/modules/Forums/index.php',
+		  //__DIR__ . '/modules/Forums/index.php',
     ]);
 
     // register a single rule
     $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
 
         $rectorConfig->sets([
-            LevelSetList::UP_TO_PHP_72
+            LevelSetList::UP_TO_PHP_82
         ]);
 };
