51a76e79890f12f8d61d7cb0a04bf0ba1cf322ae
Ernest Buffington <ernest.buffington@gmail.com>
2023-03-04 21:40:26 -0500

    Update rector.php

--- rector.php
+++ rector.php
@@ -8,7 +8,7 @@ use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-		  __DIR__ . '/admin/modules/amazon.php',
+		  __DIR__ . '/includes/nukesentinel.php',
     ]);
 
     // register a single rule
