0db55a51215d7b2ad20162d70e6ad82f2fcdd0c8
Ernest Buffington <ernest.buffington@gmail.com>
2023-03-05 16:20:55 -0500

    Update lang-english.php

--- modules/Shout_Box/lang-block/lang-english.php
+++ modules/Shout_Box/lang-block/lang-english.php
@@ -11,29 +11,29 @@
 // the Free Software Foundation
 // ===========================================
 
-define("_XXXBLOCKED",".XXX URLs blocked");
-define("_NICKTOOSHORT","Nickname too short.");
-define("_NAME","Name");
-define("_NONICK","No nickname.");
-define("_SHOUTTOOSHORT","Shout too short.");
-define("_SHOUTTOOLONG","Shout too long.");
-define("_NOSHOUT","No shout.");
-define("_SB_MESSAGE","Message");
-define("_JSINSHOUT","JavaScript in shout.");
-define("_URLNOTALLOWED","URLs not allowed in shouts.");
-define("_BANNEDNICK","Banned nickname.");
-define("_NOCLONINGNICKS","Cloning a registered nickname is not allowed.");
-define("_DUPLICATESHOUT","Duplicate shout");
-define("_ONLYREGISTERED2","Only registered users can shout.");
-define("_SB_NOTE","Note");
-define("_SB_ADMIN","Admin");
-define("_SHOUTHISTORY","Shout History");
-define("_ONLYREGISTERED","Only registered users can shout. Please");
-define("_SHOUTLOGIN","login");
-define("_OR","or");
-define("_CREATEANACCT","create an account");
-define("_SHOUT","Shout");
-define("_SMILIES","Smilies");
-define("_YOUAREBANNED","You are<br />BANNED!");
+define_once("_XXXBLOCKED",".XXX URLs blocked");
+define_once("_NICKTOOSHORT","Nickname too short.");
+define_once("_NAME","Name");
+define_once("_NONICK","No nickname.");
+define_once("_SHOUTTOOSHORT","Shout too short.");
+define_once("_SHOUTTOOLONG","Shout too long.");
+define_once("_NOSHOUT","No shout.");
+define_once("_SB_MESSAGE","Message");
+define_once("_JSINSHOUT","JavaScript in shout.");
+define_once("_URLNOTALLOWED","URLs not allowed in shouts.");
+define_once("_BANNEDNICK","Banned nickname.");
+define_once("_NOCLONINGNICKS","Cloning a registered nickname is not allowed.");
+define_once("_DUPLICATESHOUT","Duplicate shout");
+define_once("_ONLYREGISTERED2","Only registered users can shout.");
+define_once("_SB_NOTE","Note");
+define_once("_SB_ADMIN","Admin");
+define_once("_SHOUTHISTORY","Shout History");
+define_once("_ONLYREGISTERED","Only registered users can shout. Please");
+define_once("_SHOUTLOGIN","login");
+define_once("_OR","or");
+define_once("_CREATEANACCT","create an account");
+define_once("_SHOUT","Shout");
+define_once("_SMILIES","Smilies");
+define_once("_YOUAREBANNED","You are<br />BANNED!");
 
 ?>
