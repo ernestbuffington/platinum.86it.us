db8e4dc857826b503f4cb6c269bf9d3fe12a8060
Ernest Buffington <ernest.buffington@gmail.com>
2023-03-05 16:06:49 -0500

    Update rector.php

--- rector.php
+++ rector.php
@@ -9,7 +9,8 @@ use Rector\Set\ValueObject\LevelSetList;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
 		  //__DIR__ . '/db/mysqli.php',
-		  __DIR__ . '/themes/Titanium/theme.php',
+		  //__DIR__ . '/themes/Titanium/theme.php',
+		  __DIR__ . '/includes/kses/kses.php',
 		  //__DIR__ . '/includes/nukesentinel.php',
 		  //__DIR__ . '/mainfile.php', PHP 8.2.3
 		  //__DIR__ . '/modules/Forums/common.php',
