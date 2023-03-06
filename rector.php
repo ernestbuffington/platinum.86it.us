314bf2ba61cca0d1f4927d034ca1e7c2f96746f4
Ernest Buffington <ernest.buffington@gmail.com>
2023-03-06 14:15:00 -0500

    Update rector.php

--- rector.php
+++ rector.php
@@ -20,7 +20,7 @@ return static function (RectorConfig $rectorConfig): void {
 		  //__DIR__ . '/modules/Forums/index.php',
 		  //__DIR__ . '/includes/nsngr_func.php',
 		  //__DIR__ . '/modules/Your_Account/includes/functions.php',
-		    __DIR__ . '/modules/Your_Account/public/userinfo.php',
+		  //__DIR__ . '/modules/Your_Account/public/userinfo.php',
 		  //__DIR__ . '/includes/classes/class.paginator_html.php',
 		  //__DIR__ . '/modules/News/index.php',
 		  //__DIR__ . '/modules/News/article.php',
