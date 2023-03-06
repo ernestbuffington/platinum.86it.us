c890651dfd00da846b61bb3b473f1c0e283459c7
Ernest Buffington <ernest.buffington@gmail.com>
2023-03-06 02:07:02 -0500

    Update install-english.php

--- modules/Universal/language/install-english.php
+++ modules/Universal/language/install-english.php
@@ -1,5 +1,4 @@
 <?php
-
 #####################################################
 #													#
 #	Universal Module 2.5							#
@@ -39,80 +38,78 @@
 /* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
 /*******************************************************************************/
 
-define("_UMIINSTALLTITLE","Universal Module Installation");
-define("_UMINOTALLOWED","You are not allowed to access this file");
+define_once("_UMIINSTALLTITLE","Universal Module Installation");
+define_once("_UMINOTALLOWED","You are not allowed to access this file");
 
-define("_UMIWARNBIG","WARNING");
-define("_UMICANTWRITE1","I cannot write to");
-define("_UMICANTWRITE2","Please make this file writable else the install will not be able to be completed");
-define("_UMICANTWRITE3","Please");
-define("_UMICANTWRITE4","to try again after you make the file writable");
-define("_UMICLICKHERE","Click Here");
+define_once("_UMIWARNBIG","WARNING");
+define_once("_UMICANTWRITE1","I cannot write to");
+define_once("_UMICANTWRITE2","Please make this file writable else the install will not be able to be completed");
+define_once("_UMICANTWRITE3","Please");
+define_once("_UMICANTWRITE4","to try again after you make the file writable");
+define_once("_UMICLICKHERE","Click Here");
 
-define("_UMIINSTALLTITLE2","Universal Module 2.5 Installation System");
-define("_UMINEWINSTALL","New Install");
-define("_UMINEWINSTALLMESS1","Click this button, if you are installing for the first time");
-define("_UMINEWINSTALLMESS2","This will drop all existing tables");
-define("_UMINEWINSTALLMESS3","If they have your chosen prefix");
-define("_UMIUPGRADEFROM","Upgrade from");
-define("_UMIUPGRADE1MESS","Click this button, if you are upgrading from");
+define_once("_UMIINSTALLTITLE2","Universal Module 2.5 Installation System");
+define_once("_UMINEWINSTALL","New Install");
+define_once("_UMINEWINSTALLMESS1","Click this button, if you are installing for the first time");
+define_once("_UMINEWINSTALLMESS2","This will drop all existing tables");
+define_once("_UMINEWINSTALLMESS3","If they have your chosen prefix");
+define_once("_UMIUPGRADEFROM","Upgrade from");
+define_once("_UMIUPGRADE1MESS","Click this button, if you are upgrading from");
 
-define("_UMITYPEMAINPREFIX","Please type your main prefix for your database");
-define("_UMIDEFAULTPREFIX","If you do not enter one the default 'items' will be used");
-define("_UMICONTINUEINSTALL","Continue Installation");
+define_once("_UMITYPEMAINPREFIX","Please type your main prefix for your database");
+define_once("_UMIDEFAULTPREFIX","If you do not enter one the default 'items' will be used");
+define_once("_UMICONTINUEINSTALL","Continue Installation");
 
-define("_MAINCMESS","Universal Module Configuration");
-define("_MODTITLE","Module Title");
-define("_RIGHTBLOCKS","Right Blocks");
-define("_LOGOIMAGE","Logo Image");
-define("_ITEMSPERPAGE","Items Per Page");
-define("_ALLOWUSERSUBMIT","Allow User Submit");
-define("_ITEMSONPAGE","How many items on new page");
-define("_VIEWSPOPULAR","Views for a item to be popular");
-define("_ONPOPULARPAGE","How many items on Popular Page");
-define("_MAXSEARCHRESULTS","Maximum Search Results Displayed");
-define("_SHOWITEMQUEUE","Show Waiting Item Queue on Index");
-define("_ONLYREGUSERS","Only Registered Users can submit Items");
-define("_SUBMITMODIFYR","Allow Users to Submit Modify Requests");
-define("_IMAGEUPLOADUSERS","Allow Image Upload for Users");
-define("_RESTRICTIMAGEUPLOAD","Restrict Image Upload to Registered Users");
-define("_ALLOWCOMMENTS","Allow Comment Posting");
-define("_RESTRICTCOMMENTS","Restrict Comment Posting to Registered Users");
-define("_MAXTOPRATED","Max. Num of Items on Top Rated Page");
-define("_MAINPREFIX","Main Database Prefix");
-define("_MOSTPOPBLOCK","Most Popular Block On Module Index");
-define("_NEWBLOCK","New Block on Module Index");
-define("_MAXSUBCATS","Max. Sub-Categories displayed under a main one");
-define("_ALLOWRATINGS","Allow Items to be rated");
-define("_MOSTWANTEDSYSTEM","Most Wanted System");
-define("_MWPOSTINGLEVEL","Most Wanted System Posting Level");
-define("_SORTBYTYPE","Sort By Type");
-define("_MWPERPAGE","Items Per Page for Most Wanted Section");
-define("_SAVESETTINGS","Save Settings");
-define("_ADYES","Yes");
-define("_ADNO","No");
-define("_ADON","On");
-define("_ADOFF","Off");
-define("_EVERYONE","Everyone");
-define("_REGUSERS","Reg. Users");
-define("_ADMINONLY","Admin's Only");
-define("_DROPDOWNBOX","Drop-Down Box");
-define("_TEXTLINKS","Text Links");
-define("_QUICKVIEW","Quickview on Module Index");
-define("_QUICKVIEWNUM","Number of Items the Quickview Function Displays");
-define("_RANDOMQUICK","Make Quickview display random items");
-define("_QVARTICLE","Quickview displays a sample of content");
-define("_QVACHARLIMIT","Number of characters that quickview displays of the content");
-define("_CATUSEDESCRIP","Category index displays Items Description, if not a content sample");
-define("_LIMITMODREQUESTS","Limit Modifcation Requests to the user who submitted the item");
-define("_JSCHECKING","Disable checking for author and website when submitting items");
-define("_USEPHPBBNUMBERING","Enable phpBB Style page numbering");
-define("_USEMULTILINGUELFEATURE","Enable Multilinguel Feature");
-define("_NOSUBCATS","Disable Sub Categories");
+define_once("_MAINCMESS","Universal Module Configuration");
+define_once("_MODTITLE","Module Title");
+define_once("_RIGHTBLOCKS","Right Blocks");
+define_once("_LOGOIMAGE","Logo Image");
+define_once("_ITEMSPERPAGE","Items Per Page");
+define_once("_ALLOWUSERSUBMIT","Allow User Submit");
+define_once("_ITEMSONPAGE","How many items on new page");
+define_once("_VIEWSPOPULAR","Views for a item to be popular");
+define_once("_ONPOPULARPAGE","How many items on Popular Page");
+define_once("_MAXSEARCHRESULTS","Maximum Search Results Displayed");
+define_once("_SHOWITEMQUEUE","Show Waiting Item Queue on Index");
+define_once("_ONLYREGUSERS","Only Registered Users can submit Items");
+define_once("_SUBMITMODIFYR","Allow Users to Submit Modify Requests");
+define_once("_IMAGEUPLOADUSERS","Allow Image Upload for Users");
+define_once("_RESTRICTIMAGEUPLOAD","Restrict Image Upload to Registered Users");
+define_once("_ALLOWCOMMENTS","Allow Comment Posting");
+define_once("_RESTRICTCOMMENTS","Restrict Comment Posting to Registered Users");
+define_once("_MAXTOPRATED","Max. Num of Items on Top Rated Page");
+define_once("_MAINPREFIX","Main Database Prefix");
+define_once("_MOSTPOPBLOCK","Most Popular Block On Module Index");
+define_once("_NEWBLOCK","New Block on Module Index");
+define_once("_MAXSUBCATS","Max. Sub-Categories displayed under a main one");
+define_once("_ALLOWRATINGS","Allow Items to be rated");
+define_once("_MOSTWANTEDSYSTEM","Most Wanted System");
+define_once("_MWPOSTINGLEVEL","Most Wanted System Posting Level");
+define_once("_SORTBYTYPE","Sort By Type");
+define_once("_MWPERPAGE","Items Per Page for Most Wanted Section");
+define_once("_SAVESETTINGS","Save Settings");
+define_once("_ADYES","Yes");
+define_once("_ADNO","No");
+define_once("_ADON","On");
+define_once("_ADOFF","Off");
+define_once("_EVERYONE","Everyone");
+define_once("_REGUSERS","Reg. Users");
+define_once("_ADMINONLY","Admin's Only");
+define_once("_DROPDOWNBOX","Drop-Down Box");
+define_once("_TEXTLINKS","Text Links");
+define_once("_QUICKVIEW","Quickview on Module Index");
+define_once("_QUICKVIEWNUM","Number of Items the Quickview Function Displays");
+define_once("_RANDOMQUICK","Make Quickview display random items");
+define_once("_QVARTICLE","Quickview displays a sample of content");
+define_once("_QVACHARLIMIT","Number of characters that quickview displays of the content");
+define_once("_CATUSEDESCRIP","Category index displays Items Description, if not a content sample");
+define_once("_LIMITMODREQUESTS","Limit Modifcation Requests to the user who submitted the item");
+define_once("_JSCHECKING","Disable checking for author and website when submitting items");
+define_once("_USEPHPBBNUMBERING","Enable phpBB Style page numbering");
+define_once("_USEMULTILINGUELFEATURE","Enable Multilinguel Feature");
+define_once("_NOSUBCATS","Disable Sub Categories");
 
-define("_UMIINSTALLCOMPLETE","Install Complete");
-define("_UMIPLEASEDELETE","Please delete this file immediately to prevent anyone");
-define("_UMIRESETDATABASE","resetting your database");
-define("_UMIUPGRADECOMPLETE","Upgrade Complete");
-
-?>
+define_once("_UMIINSTALLCOMPLETE","Install Complete");
+define_once("_UMIPLEASEDELETE","Please delete this file immediately to prevent anyone");
+define_once("_UMIRESETDATABASE","resetting your database");
+define_once("_UMIUPGRADECOMPLETE","Upgrade Complete");
