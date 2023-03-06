0349dcb58364863db42d0b62e99632368f89095c
Ernest Buffington <ernest.buffington@gmail.com>
2023-03-06 01:46:27 -0500

    Update lang-english.php

--- modules/Contact/language/lang-english.php
+++ modules/Contact/language/lang-english.php
@@ -1,5 +1,4 @@
 <?php
-
 /************************************************************************/
 /* Contact Plus V2.2b for Nuke 6.5 - 7.2                                */
 /* Copyright (c) 2004 by Shawn Archer                                   */
@@ -63,45 +62,44 @@
 /* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
 /*******************************************************************************/
 
-define("_CON_ADM_LNK","Contact Us");
+define_once("_CON_ADM_LNK","Contact Us");
 
-define("_CONTACTUS","Contact Us");
-define("_THANKYOUFOR","Thank you for Contacting");
-define("_ERROR2","There was an error sending your email.");
-define("_TRYAGAIN","We are sorry, please try again later.");
-define("_ERROR","<font color=\"#CC0000\">The email address you entered is invalid!</font>");
-define("_GETBACK","We will get back with you as soon as possible.");
-define("_EMAILSENT","Your email message was sent successfully.");
-define("_BACK","Go Back");
-define("_FORMHEADER","Use this form to Contact<br>any Department at");
-define("_INVALIDEMAIL","The email address");
-define("_INVALIDEMAIL2","is invalid.");
-define("_NOMESSAGE","You didn't write a message!");
-define("_PLEASEGO","Please <a href=\"javascript:history.go(-1)\">Go Back</a> and enter a valid email address.");
-define("_PLEASEGO2","Please <a href=\"javascript:history.go(-1)\">Go Back</a> and enter a message.");
-define("_PLEASEGO3","Please <a href=\"javascript:history.go(-1)\">Go Back</a> and select a Department.");
-define("_YOURNAME","Your Name");
-define("_OPTIONAL","Optional");
-define("_YOUREMAIL","Your Email");
-define("_YOURMESSAGE","Your Message");
-define("_PLEASESELECT","Select Department");
-define("_SEND","Send");
-define("_CLEAR","Clear");
-define("_MAILINGINFO","Mailing Information");
-define("_PHONEANDFAX","Phone & Fax Information");
-define("_DEPARTMENT","Department");
-define("_TODEPARTMENT","To Department");
-define("_SELECTDEPARTMENT","You did not select a Contact Department.");
-define("_CONTACTFORM","Contact Form");
-define("_FROM","From");
-define("_VISITOR","From Visitor");
-define("_MESSAGE","Message");
-define("_CONTACTMESSSENT","Contace Message Sent");
-define("_ADDRESSINFO","Address Information");
-define("_PHONEINFO","Available Contact Numbers");
-define("_NSPHONENAME","Phone Name");
-define("_NSPHONENUM","Phone Number");
-define("_NSFAXNUM","Fax Number");
+define_once("_CONTACTUS","Contact Us");
+define_once("_THANKYOUFOR","Thank you for Contacting");
+define_once("_ERROR2","There was an error sending your email.");
+define_once("_TRYAGAIN","We are sorry, please try again later.");
+define_once("_ERROR","<font color=\"#CC0000\">The email address you entered is invalid!</font>");
+define_once("_GETBACK","We will get back with you as soon as possible.");
+define_once("_EMAILSENT","Your email message was sent successfully.");
+define_once("_BACK","Go Back");
+define_once("_FORMHEADER","Use this form to Contact<br>any Department at");
+define_once("_INVALIDEMAIL","The email address");
+define_once("_INVALIDEMAIL2","is invalid.");
+define_once("_NOMESSAGE","You didn't write a message!");
+define_once("_PLEASEGO","Please <a href=\"javascript:history.go(-1)\">Go Back</a> and enter a valid email address.");
+define_once("_PLEASEGO2","Please <a href=\"javascript:history.go(-1)\">Go Back</a> and enter a message.");
+define_once("_PLEASEGO3","Please <a href=\"javascript:history.go(-1)\">Go Back</a> and select a Department.");
+define_once("_YOURNAME","Your Name");
+define_once("_OPTIONAL","Optional");
+define_once("_YOUREMAIL","Your Email");
+define_once("_YOURMESSAGE","Your Message");
+define_once("_PLEASESELECT","Select Department");
+define_once("_SEND","Send");
+define_once("_CLEAR","Clear");
+define_once("_MAILINGINFO","Mailing Information");
+define_once("_PHONEANDFAX","Phone & Fax Information");
+define_once("_DEPARTMENT","Department");
+define_once("_TODEPARTMENT","To Department");
+define_once("_SELECTDEPARTMENT","You did not select a Contact Department.");
+define_once("_CONTACTFORM","Contact Form");
+define_once("_FROM","From");
+define_once("_VISITOR","From Visitor");
+define_once("_MESSAGE","Message");
+define_once("_CONTACTMESSSENT","Contace Message Sent");
+define_once("_ADDRESSINFO","Address Information");
+define_once("_PHONEINFO","Available Contact Numbers");
+define_once("_NSPHONENAME","Phone Name");
+define_once("_NSPHONENUM","Phone Number");
+define_once("_NSFAXNUM","Fax Number");
 
-?>
 
