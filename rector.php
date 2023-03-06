ace97b29abb15bccff5155d0d0d137a43ba22e16
Ernest Buffington <ernest.buffington@gmail.com>
2023-03-06 14:16:46 -0500

    Update rector.php

--- rector.php
+++ rector.php
@@ -8,26 +8,26 @@ use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-		  //__DIR__ . '/db/mysqli.php',
-		  //__DIR__ . '/modules.php',
-		  //__DIR__ . '/themes/Titanium/theme.php',
-		  //__DIR__ . '/includes/kses/kses.php',
-		  //__DIR__ . '/blocks/block-Shout_Box.php',
-		  //__DIR__ . '/blocks/block-ForumsCollapsing.php',
-		  //__DIR__ . '/includes/nukesentinel.php',
+		  //__DIR__ . '/db/mysqli.php', # PHP 8.2.3
+		  //__DIR__ . '/modules.php', # PHP 8.2.3
+		  //__DIR__ . '/themes/Titanium/theme.php', # PHP 8.2.3
+		  //__DIR__ . '/includes/kses/kses.php', # PHP 8.2.3
+		  //__DIR__ . '/blocks/block-Shout_Box.php', # PHP 8.2.3
+		  //__DIR__ . '/blocks/block-ForumsCollapsing.php', # PHP 8.2.3
+		  //__DIR__ . '/includes/nukesentinel.php', # PHP 8.2.3
 		  //__DIR__ . '/mainfile.php', # PHP 8.2.3
-		  //__DIR__ . '/modules/Forums/common.php',
-		  //__DIR__ . '/modules/Forums/index.php',
-		  //__DIR__ . '/includes/nsngr_func.php',
-		  //__DIR__ . '/modules/Your_Account/includes/functions.php',
-		  //__DIR__ . '/modules/Your_Account/public/userinfo.php',
-		  //__DIR__ . '/includes/classes/class.paginator_html.php',
-		  //__DIR__ . '/modules/News/index.php',
-		  //__DIR__ . '/modules/News/article.php',
-		  //__DIR__ . '/index.php',
-		  //__DIR__ . '/themes/Impressed/theme.php',
-		  //__DIR__ . '/modules/Advertising/index.php',
-		  //__DIR__ . '/modules/Advertising/admin/index.php',
+		  //__DIR__ . '/modules/Forums/common.php', # PHP 8.2.3
+		  //__DIR__ . '/modules/Forums/index.php', # PHP 8.2.3
+		  //__DIR__ . '/includes/nsngr_func.php', # PHP 8.2.3
+		  //__DIR__ . '/modules/Your_Account/includes/functions.php', # PHP 8.2.3
+		  //__DIR__ . '/modules/Your_Account/public/userinfo.php', # PHP 8.2.3
+		  //__DIR__ . '/includes/classes/class.paginator_html.php', # PHP 8.2.3
+		  //__DIR__ . '/modules/News/index.php', # PHP 8.2.3
+		  //__DIR__ . '/modules/News/article.php', # PHP 8.2.3
+		  //__DIR__ . '/index.php', # PHP 8.2.3
+		  //__DIR__ . '/themes/Impressed/theme.php', # PHP 8.2.3
+		  //__DIR__ . '/modules/Advertising/index.php', # PHP 8.2.3
+		  //__DIR__ . '/modules/Advertising/admin/index.php', # PHP 8.2.3
     ]);
 
     // register a single rule
